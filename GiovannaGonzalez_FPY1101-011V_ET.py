402c312e37ff171f0628b18c9f72b86d29f12a0b
gigonzalezc <gi.gonzalezc@duocuc.cl>
2024-07-15 22:09:08 -0400

    Add files via upload
    Envió Archivo

---
+++ GiovannaGonzalez_FPY1101-011V_ET.py
@@ -0,0 +1,52 @@
+print= ("bienvenido a syg")
+
+list=()
+list= 'Archivo'.csv =(open('nombre_apellido'))
+Datos=("ingresar datos trabajadores")
+
+def= ('ingrese_trabajadores)
+      for=
+while= in ('trabajadores')
+        try= ('int_(gresar_datos' )
+print=('Juan_Perez','sueldo: 300000','maria_Garcia','sueldo:350000','Carlos_Lopez','sueldo:400000')
+print=('Ana_Martinez','sueldo:450000','Pedro_Rodriguez''sueldo:550000','Laura_Hernandez','sueldo:690000','Miguel_Sanchez','sueldo:750000','Isabel_Gomez''sueldo:1500000','Elena_Fernandez','sueldo:2500000')
+    def=(detalle_liquidacion)
+continue= ingresar_trabajadores
+
+try():
+    for=detalle_liquidacion(input(ingresar_datos))
+    else=(ingresar datos)
+    else=(Monto_base)
+    else=(cotizacion_salud7%) 
+    else=(cotizacion afp12%)
+    break=("salir")
+       
+valor= sueldo_trabajador
+input=("sueldo_menor a mayor" )
+print=('Juan_Perez','sueldo: 300000','maria_Garcia','sueldo:350000','Carlos_Lopez','sueldo:400000')
+print=('Ana_Martinez','sueldo:450000','Pedro_Rodriguez''sueldo:550000','Laura_Hernandez','sueldo:690000','Miguel_Sanchez','sueldo:750000','Isabel_Gomez''sueldo:1500000','Elena_Fernandez','sueldo:2500000')
+def=('nombre_apellido')
+print=('nombre_apellido')
+def=('monto_sueldo')
+print=('sueldo_lisquido')
+def=('descuento salus7%')
+print=("descuento salud 7%")
+def=('descuento 12%')
+print= ("descuentos AFP 12%")
+else=('salir funcion')
+    if= multiplicar_sueldo*7%=resultado
+    print=('resultado')
+    if= sueldo_base*12%= resultado
+    print=("resultado")
+        Valor=300000*12%=36000
+        True=in 'resultado correcto'
+        False= in ("resultado incorrecto": )
+        return=(selecionar_trabajador)
+
+===================liquidacion==================
+print=("nombre_trabajador")
+print=("sueldo_baso") 
+print=("cotizacion_salud")      
+print=("cotizacion_afp")
+print=("sueldo_liquido")
+exit= salir_programa
